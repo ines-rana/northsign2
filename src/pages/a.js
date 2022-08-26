8f6e7716ceb9a6bd1503513e121a85af1dbef8e1
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 22:25:53 -0500

    .

--- src/pages/a.js
+++ src/pages/a.js
@@ -10,7 +10,6 @@ export default () => {
   dangerouslySetInnerHTML={{
     __html: `
  console.log("123");
- if (req) console.log("server side", req.headers);
 
  if(typeof window !== 'undefined' && window.document) {
     // run this script on client-side

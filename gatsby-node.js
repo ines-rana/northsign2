2ed9b441fdc349116ede4a05e45ea4f3a1d02f84
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 23:39:44 -0500

    .

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,41 +1,9 @@
 exports.createPages = async ({ graphql, actions }) => {
   const { createRedirect } = actions;
 
-/*
-  createRedirect({ fromPath: `/`, toPath: `/api/northsign/`, })
+  // !!! delete src/pages/index.js first !!!
+  //     otherwise the following will not work
 
-https://northsign.gtsb.io/?scale=13\&degrees=4\&scale=7
-
-  createRedirect({ fromPath: `/?degrees=:degrees`,
-    toPath: `/api/northsign/?degrees=:degrees` })
-
-*/
-
-/* 
-      -> This will NOT work; no scale/degrees parameters in the redirection URL
-  createRedirect({ fromPath: `/?degrees=:degrees&scale=:scale`,
-    toPath: `/api/northsign/?degrees=:degrees&scale=:scale` })
-
-  // /?y=2\x\&degrees=4\&scale=7 -> /?y=2x&degrees=4
-  createRedirect({ fromPath: `/?scale=:scale`,
-    toPath: `/api/northsign/?scale=:scale` })
-
-  // /?y=2\x\&degrees=4\&scale=7\&dummyParameter=1 -> /?y=2x&degrees=4&scale=4
-  createRedirect({ fromPath: `/?dummyParameter=:dummyParameter`,
-    toPath: `/api/northsign/?dummyParameter=:dummyParameter` })
-
-  // This will NOT work; no redirection
-  createRedirect({ fromPath: `/?`, toPath: `/api/northsign/?` })
-
-  // This will NOT work; no redirection
-  createRedirect({ fromPath: `/`, toPath: `/api/northsign/?x=2` })
-
-  // this works
-  createRedirect({ fromPath: `/index.js`, toPath: `/api/northsign/?x=2` })
-*/
-
-
-  
   createRedirect({
     fromPath: '/',
     toPath: '/api/northsign/',

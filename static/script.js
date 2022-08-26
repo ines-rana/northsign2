621de5b95e0fa557e4c3381e9e9f6a096f92f377
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 23:45:34 -0500

    .

--- static/script.js
+++
@@ -1,8 +0,0 @@
- console.log("server side", req.headers);
-
- if(typeof window !== 'undefined' && window.document) {
-    // run this script on client-side
-    console.log(window.location.href);
- }
-
-

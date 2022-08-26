c2a41584eeaae8adf3fc0aa938b3297f32b9e50b
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 23:14:20 -0500

    .

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,15 +12,19 @@ https://northsign.gtsb.io/?scale=13\&degrees=4\&scale=7
 */
 
 /* 
-      -> api, no parameters
+      -> This will NOT work; no scale/degrees parameters in the redirection URL
   createRedirect({ fromPath: `/?degrees=:degrees&scale=:scale`,
     toPath: `/api/northsign/?degrees=:degrees&scale=:scale` })
 
   // /?y=2\x\&degrees=4\&scale=7 -> /?y=2x&degrees=4
   createRedirect({ fromPath: `/?scale=:scale`,
     toPath: `/api/northsign/?scale=:scale` })
-*/
-  // /?y=2\x\&degrees=4\&scale=7 -> /?y=2x&degrees=4
+
+  // /?y=2\x\&degrees=4\&scale=7\&dummyParameter=1 -> /?y=2x&degrees=4&scale=4
   createRedirect({ fromPath: `/?dummyParameter=:dummyParameter`,
     toPath: `/api/northsign/?dummyParameter=:dummyParameter` })
+*/
+
+  createRedirect({ fromPath: `/?`,
+    toPath: `/api/northsign/?` })
 }

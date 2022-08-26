ddf145bb5e3c831356ad41186101b91f989ef6ff
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-26 00:08:42 -0500

    .

---
+++ src/pages/index_deleted.js
@@ -0,0 +1,9 @@
+// !!! delete src/pages/index.js !!!
+//     otherwise the "/" --> "/whatever" redirection will not work
+import * as React from "react"
+
+export default () => {
+  return (
+<div>Hi</div>
+  )
+}

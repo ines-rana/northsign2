b4c7bc318434f2d7d42531a9e329673e9ab47c07
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 21:02:08 -0500

    .

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,11 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createRedirect } = actions;
 
   createRedirect({
+    fromPath: `/`,
+    toPath: `/api/northsign`,
+  })
+
+  createRedirect({
     fromPath: `/?degrees=:degrees&scale=:scale`,
     toPath: `/api/northsign?degrees=:degrees&scale=:scale`,
   })

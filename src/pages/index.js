19b5cb739de9d23f3b6d1818486391ade9f27269
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 19:56:32 -0500

    .

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Helmet } from "react-helmet"
+
+
+export default () => {
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta http-equiv = "refresh" content = "0; url = /index_s.html" />
+      </Helmet>
+
+    </React.Fragment>
+  )
+}

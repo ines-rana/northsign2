d08567c51aa11f8d05a151b833cdefd1806c32a6
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 22:35:01 -0500

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import * as React from "react"
-import { Helmet } from "react-helmet"
 
+// dynamic redirection
 
 export default () => {
   return (
-<div> HI </div>
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+
+ if(typeof window !== 'undefined' && window.document) {
+    // run this script on client-side
+    var old_url = window.location.href; 
+    var new_url = old_url
+      .replace("/index.html/","/api/northsign/")
+      .replace("/?","/api/northsign/?")
+    );
+ }
+
+      var foo = 'bar';
+      console.log("redirection from %s to %s", old_url, new_url);
+//    window.location = new_url;
+    `,
+  }}
+/>
   )
 }

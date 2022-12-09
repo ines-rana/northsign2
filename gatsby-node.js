47aaf7ea7be594847a7790c5d512cb7b6bf06562
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-12-09 03:57:37 -0600

    .

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,10 +3,12 @@ exports.createPages = async ({ graphql, actions }) => {
 
   // !!! delete src/pages/index.js first !!!
   //     otherwise the following will not work
+  //toPath: '/api/northsign/',
+  //toPath: 'https://northsign.gatsbyjs.io/api/northsign/',
 
   createRedirect({
     fromPath: '/',
-    toPath: 'https://northsign.gatsbyjs.io/api/northsign/',
+    toPath: 'https://northsign.gtsb.io/api/northsign/',
     isPermanent: true,
     redirectInBrowser: true,
  })
